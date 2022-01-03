5bcd24f7bc1eb4b1a758cbc13bc3a149791cdd70
RyryTheGuy <ryanroemer61@gmail.com>
2022-01-03 10:31:12 -0600

    Removed unnessary npm packages

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import cors from 'cors';
 import { connectToMongoDB } from './utils/mongodb';
 import config from './utils/config';
 import middleware from './utils/middleware';
@@ -8,10 +7,8 @@ require('express-async-errors');
 import imageRouter from './routes/images';
 
 const app = express();
-const options: cors.CorsOptions = { origin: ['http://localhost:3000'] };
 connectToMongoDB();
 
-app.use(cors(options));
 app.use(express.json());
 app.use(express.static('build'));
 
